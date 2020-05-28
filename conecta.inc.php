025f2065e9a030e437031910c877c60b0d3156ce
Renato Matos <62756678+renatomsma@users.noreply.github.com>
2020-05-28 10:48:15 -0300

    Add files via upload

--- conecta.inc.php
+++ conecta.inc.php
@@ -1,12 +1,12 @@
 <?php
 // conectando ao servidor
-$conexao = mysqli_connect("localhost","root","");
+$conexao = mysqli_connect("localhost","id13745096_root","{~SszRICu7EHDA#|");
 // conectando ao BD criado
-$db = mysqli_select_db($conexao, "site");
+$db = mysqli_select_db($conexao, "id13745096_site");
 // Executando query e retornando valores da tabela "pedidos"
 $query = mysqli_query($conexao, "SELECT * FROM pedidos");
 if($conexao)
-echo "Conectado ao Banco!";
+echo "Banco de dados Online.";
 else
-echo "Sem conexão com o Banco!";
+echo "Banco de dados Offline.";
 ?>
--- contatodb.php
+++ contatodb.php
@@ -6,4 +6,5 @@
 @$mensagem = $_POST['mensagem'];
 @$dados = "INSERT INTO pedidos (nome, email,telefone,assunto,mensagem) VALUES ('$nome','$email','$telefone','$assunto','$mensagem')";
 $query = mysqli_query($conexao,$dados);
-?>
+?>   
+
