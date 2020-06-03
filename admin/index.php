9c46c8fdadead64e6e8520aa038e1fdaf49d8cfe
Renato Matos <62756678+renatomsma@users.noreply.github.com>
2020-06-03 11:18:45 -0300

    Projeto Final

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+include "config.inc.php";
+?>
+</html>
+<!DOCTYPE html>
+<head>
+  <title>Código 88</title>
+  <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon"/>
+  <meta charset="UTF-8" language="pt-br">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style>
+<?php include_once('estilos.php');?>
+</style>
+<body>
+<?php include_once('topo.php');?>
+</header>
+<div class="row">
+<?php include_once('principal.php');?>   
+</div>
+</body>
+<?php include_once('rodape.php');?>
+</html>
